7751a2c4f24ecb15e81b4bb81375cc068d6d35e0
Valdo Jeremi <namesevem@gmail.com>
2021-03-30 20:41:11 +0700

    commit pertama

---
+++ home.php
@@ -0,0 +1,7 @@
+<center><br><br><br><br><br><br><br><br>
+
+<img src="assets/images/Logo-ars.png" width="200px height="200px" /> <br>
+
+<font Size="6" face="Helvetica">Sistem Informasi Akademik</font> <br>
+<font Size="6">Ars University</font>
+</center>
